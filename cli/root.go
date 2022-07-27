b55868d22f003407c400011fcff9a3af6366e7c1
Jennifer Power <barnabei.jennifer@gmail.com>
2022-07-27 14:56:10 -0400

    chore: fixes grammar and spelling in root long description
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- cli/root.go
+++ cli/root.go
@@ -24,8 +24,8 @@ var clientLong = templates.LongDesc(
 	`
 	The UOR client helps you build, publish, and retrieve UOR collections as an OCI artifact.
 
-	The workflow to publish a collection is to to build gather files to for a collection in a 
-	directory workspace and use the build sub-command. During the build process with when the tag for the
+	The workflow to publish a collection is to gather files for a collection in a directory workspace 
+	and use the build sub-command. During the build process, the tag for the
 	remote destination is specified. 
 	
 	This build action will store the collection in a build cache. This location can be specified with the UOR_CACHE environment 
@@ -33,7 +33,7 @@ var clientLong = templates.LongDesc(
 	
 	After the collection has been stored, it can be retrieved and pushed the to registry with the push sub-command.
 
-	Collections can be retrieved from the cache or the remote location (if not stored) will the pull command. The pull sub-command also
+	Collections can be retrieved from the cache or the remote location (if not stored) with the pull sub-command. The pull sub-command also
 	allows for filtering of the collection with the attributes flag.
 	`,
 )
