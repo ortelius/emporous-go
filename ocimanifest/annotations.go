b19a269d70d71acfe7f870aff7edf1ae0eff4c13
Jennifer Power <barnabei.jennifer@gmail.com>
2022-08-24 17:01:58 -0400

    fix: remove unused imports from annotations.go and empty file from docs
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- ocimanifest/annotations.go
+++ ocimanifest/annotations.go
@@ -7,9 +7,6 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/uor-framework/uor-client-go/api/v1alpha1"
-	"github.com/uor-framework/uor-client-go/config"
-
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 
 	"github.com/uor-framework/uor-client-go/attributes"
